c74f1c97655e90af2a1003a74195546244f10769
Vinicius-renesto <vini.reenesto@gmail.com>
2025-04-04 17:58:09 -0300

    Add files via upload

---
+++ Aula-8-Data Science -Vinícius.py
@@ -0,0 +1,116 @@
+import numpy as np 
+'''array_2d = np.random.randint(0, 100, size=(5, 5))
+print(array_2d)
+
+soma = sum(array_2d[0])
+m = soma /len(array_2d[0])
+print(m)
+soma = sum(array_2d[1])
+m = soma /len(array_2d[1])
+print(m)
+
+soma = sum(array_2d[2])
+m = soma /len(array_2d[2])
+print(m)
+
+soma = sum(array_2d[3])
+m = soma /len(array_2d[3])
+print(m)
+
+soma = sum(array_2d[4])
+m = soma /len(array_2d[4])
+print(m)
+
+print ('maximo e minimo')
+maximo = np.max(array_2d)
+minimo = np.min(array_2d)
+print(maximo)
+print(minimo)'''
+
+
+# Desafio 2 
+
+'''vendas = np.array([120,90,150,80,200,110,50,300])
+filtro = vendas > 100
+x = vendas[filtro]
+print(x)
+
+m = np.median(vendas)
+abaixo_da_media = len(vendas[vendas < m])
+print(abaixo_da_media)
+
+vendas_normalizadas = vendas / np.max(vendas)
+print(vendas_normalizadas)
+'''
+
+# Aula-8-Parte 2
+
+#Desafio 1:
+
+'''array_2d = np.random.randint(0, 100, size=(4, 5))
+print(array_2d)
+
+primeiros_5 = array_2d[0]
+print(primeiros_5)
+ultimos_5 = array_2d[3]
+print(ultimos_5)
+elementos_5_a_10 = array_2d[4:11]
+print(elementos_5_a_10)'''
+
+#Desafio 2
+
+'''array_2d = np.random.randint(0, 100, size=(3, 3))
+print(array_2d)
+print('-----')
+array_2d1 = np.random.randint(0, 100, size=(3, 3))
+print(array_2d1)
+print('-----')
+mult = np.dot(array_2d,array_2d1)
+print(mult)'''
+
+# Desafioc 3
+
+'''array_2d = np.arange(1, 10)
+print(array_2d)
+
+array_2d = np.random.rand(3, 3)
+print(array_2d)'''
+
+# Desafio 4
+
+'''array_2d = np.random.randint(0, 100, size=(3, 3))
+print(array_2d)
+print('-----')
+soma = np.sum(array_2d)
+print(soma)
+
+maximo = np.max(array_2d)
+print(maximo)
+minimo = np.min(array_2d)
+print(minimo)'''
+
+# Desafio 5
+
+'''array_2d = np.random.randint(0, 100, size=(3, 3))
+print(array_2d)
+print('-----')
+
+media = np.mean(array_2d) 
+print(media)
+
+mediana = np.median(array_2d)
+print(mediana)'''
+
+# Desafio 6 
+'''
+array_2d = np.random.randint(0, 100, size=(3, 3))
+print(array_2d)
+print('-----')
+
+soma = array_2d + 10
+print(soma)
+print('-----')
+
+array = np.arange(1, 11)
+array_2d2 = array.reshape(2, 5)
+print(array_2d2)'''
